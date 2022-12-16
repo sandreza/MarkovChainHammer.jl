e1a005f3abd5ff411109303147f1fd571725a900
Andre Souza <andrenogueirasouza@gmail.com>
2022-12-16 16:39:36 -0500

    generate chain

---
+++ examples/generate_chain.jl
@@ -0,0 +1,46 @@
+using MarkovChainHammer
+using MarkovChainHammer.Trajectory: generate
+using MarkovChainHammer.TransitionMatrix: generator, perron_frobenius
+using MarkovChainHammer.TransitionMatrix: steady_state 
+
+using LinearAlgebra: eigen, norm
+
+
+# Markov Chain Transition Matrices
+Q = [-1/2 1/3 0.0; 1/2 -2/3 1/3; 0.0 1/3 -1/3]
+dt = 0.1
+P = exp(Q * dt)
+p_exact = steady_state(P)
+
+# Generate Markov Chain
+markov_chain = generate(P)
+
+# Generate Emperical Construction from Markov Chain
+p_empirical = [sum(markov_chain .== i) / length(markov_chain) for i in 1:3]
+Q_empirical = generator(markov_chain; dt=0.1)
+P_empirical = perron_frobenius(markov_chain)
+
+# Compare
+println("Exact steady state: "); 
+display(p_exact)
+println("Empirical steady state: "); 
+display(p_empirical)
+println("Relative Error: ", norm(p_exact - p_empirical) / norm(p_exact))
+println("--------------------------------------")
+
+# Compare
+println("Exact transfer operator: "); 
+display(P)
+println("Empirical transfer operator: "); 
+display(P_empirical)
+println("Relative Error: ", norm(P - P_empirical) / norm(P))
+println("--------------------------------------")
+
+##
+# Compare
+println("Exact generator: "); 
+display(Q)
+println("Empirical generator: "); 
+display(Q_empirical)
+println("Relative Error: ", norm(Q - Q_empirical) / norm(Q))
+println("--------------------------------------")
