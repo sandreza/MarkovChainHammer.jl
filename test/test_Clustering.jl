63d0610d2e59fa30a92d2dbc4bead0e7ea803457
Andre Souza <andrenogueirasouza@gmail.com>
2022-12-18 13:48:53 -0500

    update Leicht Newman test

--- test/test_Clustering.jl
+++ test/test_Clustering.jl
@@ -4,6 +4,7 @@ import MarkovChainHammer.Clustering: modularity_matrix, principal_vector, modula
 import MarkovChainHammer.Clustering: split_community, leicht_newman
 
 @testset "Leicht Newman" begin
+    # 3 communities
     A = [0 1 1; 1 0 1; 1 1 0]
     B = modularity_matrix(A)
     @test all(B .== [-1 -1 -1; -1 -1 -1; -1 -1 -1])
@@ -15,4 +16,18 @@ import MarkovChainHammer.Clustering: split_community, leicht_newman
     @test all(community_cut1[2] .== [2, 3])
     communities = leicht_newman(A)
     @test length(communities) == 3
+
+    # 2 communities
+    A = [1 1 0; 1 1 0; 0 0 1]
+    B = modularity_matrix(A)
+    @test all(B .≈ [0.4 -1.6 -2.4; -1.6 0.4 -2.4; -2.4 -2.4 2.4])
+    s = principal_vector(B)
+    @test all(s .== [-1, -1, 1])
+    @test modularity(B) ≈ 9.6
+    community_cut1 = split_community(B, [1, 2, 3])
+    @test all(community_cut1[1] .== [3])
+    @test all(community_cut1[2] .== [1, 2])
+    communities = leicht_newman(A)
+    @test length(communities) == 3
+
 end
