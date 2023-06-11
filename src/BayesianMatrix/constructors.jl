ff031552909f3d7a8c85d0b29e18e43fdce7df93
Andre Souza <andrenogueirasouza@gmail.com>
2023-06-11 15:06:43 -0400

    docstring for uninformative prior

--- src/BayesianMatrix/constructors.jl
+++ src/BayesianMatrix/constructors.jl
@@ -181,6 +181,22 @@ function BayesianGenerator(parameters::NamedTuple)
     return BayesianGenerator(prior, posterior, predictive)
 end
 
+"""
+    uninformative_prior(number_of_states; scale=eps(1e2))
+
+Construct an uninformative prior distribution for a BayesianGenerator object.
+
+# Arguments
+- `number_of_states::Int`: The number of states in the BayesianGenerator object.
+
+# Keyword Arguments
+
+- `scale::Number=eps(1e2)`: The scale parameter for the Gamma and Dirichlet distributions.
+
+# Returns
+- `GeneratorParameterDistributions`: An uninformative prior distribution for a BayesianGenerator object.
+
+"""
 uninformative_prior(number_of_states; scale=eps(1e2)) = GeneratorParameterDistributions(number_of_states::Int; α=scale, β=scale, αs=ones(number_of_states - 1) * scale)
 
 """
