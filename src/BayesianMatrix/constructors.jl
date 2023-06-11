4f5a57d1a7891d7eaadf01f66acd831e103007b9
Andre Souza <andrenogueirasouza@gmail.com>
2023-06-11 14:58:43 -0400

    docs

--- src/BayesianMatrix/constructors.jl
+++ src/BayesianMatrix/constructors.jl
@@ -156,6 +156,19 @@ end
 BayesianGenerator(prior::GeneratorParameterDistributions) = BayesianGenerator(prior, prior, nothing)
 BayesianGenerator(data; dt = 1) = BayesianGenerator(data,  uninformative_prior(maximum(data)) ; dt=dt)
 
+
+"""
+    BayesianGenerator(parameters::NamedTuple)
+
+Construct a BayesianGenerator object from a NamedTuple of parameters.
+
+# Arguments
+- `parameters::NamedTuple`: A NamedTuple containing the parameters for the BayesianGenerator object. Must contain the fields `prior`, `posterior` and `predictive`, each of which must be a NamedTuple containing the parameters for the respective distributions.
+
+# Returns
+- `BayesianGenerator`: A BayesianGenerator object constructed from the parameters. Contains the posterior distributions for the rates and exit probabilities, as well as the predictive distributions for the holding times and exit counts.
+
+"""
 function BayesianGenerator(parameters::NamedTuple) 
     @assert haskey(parameters, :prior)
     @assert haskey(parameters, :posterior)
@@ -170,6 +183,18 @@ end
 
 uninformative_prior(number_of_states; scale=eps(1e2)) = GeneratorParameterDistributions(number_of_states::Int; α=scale, β=scale, αs=ones(number_of_states - 1) * scale)
 
+"""
+    unpack(Q::BayesianGenerator)
+
+Unpack a BayesianGenerator object into its parameters.
+
+# Arguments
+- `Q::BayesianGenerator`: The BayesianGenerator object to unpack.
+
+# Returns
+- `Tuple{NamedTuple{(:prior, :posterior, :predictive),Tuple{NamedTuple{(:α, :β, :αs),Tuple{Vector{Float64},Vector{Float64},Vector{Float64}}},NamedTuple{(:α, :β, :αs),Tuple{Vector{Float64},Vector{Float64},Vector{Float64}}},NamedTuple{(:μ, :σ, :ξ, :n, :αs),Tuple{Vector{Float64},Vector{Float64},Vector{Float64},Vector{Int64},Vector{Vector{Float64}}}}}}}`: A tuple containing the parameters for the prior, posterior and predictive distributions.
+
+"""
 function unpack(Q::BayesianGenerator)
     prior = params(Q.prior)
     posterior = params(Q.posterior)
