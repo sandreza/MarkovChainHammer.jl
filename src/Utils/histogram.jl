90040510f7f5ace356244c02dccf1ad2df55af31
Andre Souza <andrenogueirasouza@gmail.com>
2023-12-06 12:59:48 -0800

    histogram update

--- src/Utils/histogram.jl
+++ src/Utils/histogram.jl
@@ -1,13 +1,15 @@
 export histogram
 
 """
-    histogram(array, bins=minimum([100, length(array)]), normalization=:uniform, custom_range=false)
+    histogram(array; bins=minimum([100, length(array)]), normalization=:uniform, custom_range=false)
 
 # Description
     Compute the histogram of an array. Useful for barplot in GLMakie.
 
 # Arguments
 - `array::AbstractArray`: Array to compute the histogram of.
+
+# Keyword Arguments
 - `bins::Integer`: Number of bins to use.
 - `normalization::AbstractArray`: Normalization to use. If `:uniform`, then the normalization is uniform.
 - `custom_range::Tuple`: Custom range to use. If `false`, then the range is computed from the data.
